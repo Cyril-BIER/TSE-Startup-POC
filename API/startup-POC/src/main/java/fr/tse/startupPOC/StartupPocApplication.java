bbfc976cc5e236b57ea09f8a43a484db5fa9951d
Seith42 <cyril.bier42100@gmail.com>
2024-01-13 10:47:38 +0100

    Spring App initialization

---
+++ API/startup-POC/src/main/java/fr/tse/startupPOC/StartupPocApplication.java
@@ -0,0 +1,13 @@
+package fr.tse.startupPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupPocApplication.class, args);
+	}
+
+}
---
+++ API/startup-POC/src/test/java/fr/tse/startupPOC/StartupPocApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tse.startupPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
