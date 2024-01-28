de22e264fba3d6512d48d4b2ca70dc117d76639e
jouaiti.chems <chemsjouaiti06@gmail.com>
2024-01-28 20:16:44 +0100

    création d'un utilisateur

--- WEB/src/app/utilisateur/utilisateur.component.ts
+++ WEB/src/app/utilisateur/utilisateur.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-utilisateur',
@@ -6,7 +7,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./utilisateur.component.css']
 })
 export class UtilisateurComponent {
+  searchText: string = '';
   isUserListVisible: boolean = false;
+  isCreationFormVisible: boolean = false;
+  hide = true;
+  form!: FormGroup;
   users: {
     name: string;
   }[] = [
@@ -20,9 +25,27 @@ export class UtilisateurComponent {
       name: 'User 3'
     },
   ];
-  selectedRights: string[] = ['Right 1', 'Right 3'];
+  selectedUser: string | null = null;
 
-  onNgModelChange(event: any){
+
+  constructor(private fb: FormBuilder) {
+    this.selectedUser = ''
+  }
+
+  ngOnInit(): void {
+    // Initialisation du formulaire
+    this.form = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      motDePasse: ['', Validators.required]
+    });
+  }
+
+  get filteredUsers(): any[] {
+    return this.users.filter(user => user.name.toLowerCase().includes(this.searchText.toLowerCase()));
+  }
+
+  onNgModelChange(event: any) {
     console.log('On ngModelChange : ', event);
   }
 
@@ -33,6 +56,32 @@ export class UtilisateurComponent {
   hideUsers() {
     this.isUserListVisible = false;
   }
-}
 
+  showCreation() {
+    this.isCreationFormVisible = true;
+  }
+
+  hideCreation() {
+    this.isCreationFormVisible = false;
+  }
+
+  onSubmit(): void {
+    const {nom, prenom, motDePasse} = this.form.value;
+    // Il faudra le stocker quelque part
+    this.users.push({name: nom})
+    this.isCreationFormVisible = false;
+    this.form.reset();
+  }
+
+  supprimerUtilisateur(): void {
+    if (this.selectedUser && this.selectedUser.length > 0) {
+      const selectedUserName = this.selectedUser[0]; // Prendre le premier élément du tableau
+      const index = this.users.findIndex(user => user.name === selectedUserName);
+      if (index !== -1) {
+        this.users.splice(index, 1);
+        this.selectedUser = null;
+      }
+    }
+  }
+}
 
