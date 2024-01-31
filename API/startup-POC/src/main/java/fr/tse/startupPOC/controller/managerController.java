ef209ab7940ad4e52dde3d6a2d1f24428da9de29
Cyril <cyril.bier42100@gmail.com>
2024-01-31 16:11:39 +0100

    CreateUser

--- API/startup-POC/src/main/java/fr/tse/startupPOC/controller/managerController.java
+++ API/startup-POC/src/main/java/fr/tse/startupPOC/controller/managerController.java
@@ -1,14 +1,14 @@
 package fr.tse.startupPOC.controller;
 
+import fr.tse.startupPOC.payload.request.SignupUserRequest;
+import fr.tse.startupPOC.payload.response.UserResponse;
 import fr.tse.startupPOC.service.ManagerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -23,6 +23,17 @@ public class managerController {
         try {
             return new ResponseEntity<>(managerService.getAttachedUsers(), HttpStatus.OK);
         }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/registerUser")
+    @PreAuthorize("hasRole('MANAGER')")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupUserRequest request){
+        try {
+            UserResponse response = managerService.createUser(request);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch(Exception e){
             return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- API/startup-POC/src/main/java/fr/tse/startupPOC/service/ManagerService.java
+++ API/startup-POC/src/main/java/fr/tse/startupPOC/service/ManagerService.java
@@ -2,20 +2,36 @@ package fr.tse.startupPOC.service;
 
 import fr.tse.startupPOC.models.Manager;
 import fr.tse.startupPOC.models.User;
+import fr.tse.startupPOC.payload.request.SignupUserRequest;
 import fr.tse.startupPOC.payload.response.UserResponse;
 import fr.tse.startupPOC.repository.ManagerRepository;
+import fr.tse.startupPOC.repository.ProfileRepository;
+import fr.tse.startupPOC.repository.UserRepository;
 import fr.tse.startupPOC.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.naming.AuthenticationException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ManagerService {
     @Autowired
     ManagerRepository managerRepository;
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
 
     public List<UserResponse> getAttachedUsers(){
         List<UserResponse> response = new ArrayList<>();
@@ -28,5 +44,31 @@ public class ManagerService {
             response.add(new UserResponse(user));
         }
         return response;
+    }
+
+    @Transactional
+    public UserResponse createUser(SignupUserRequest request) throws Exception {
+        if(profileRepository.existsByEmail(request.getEmail())){
+            throw new AuthenticationException("Email already taken");
+        }
+        UserDetailsImpl userDetails =
+                (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<Manager> manager = managerRepository.findById(userDetails.getId());
+        if(manager.isPresent()){
+            User user = new User(
+                    request.getEmail(),
+                    request.getFirstName(),
+                    request.getLastName(),
+                    encoder.encode(request.getPassword()),
+                    manager.get()
+            );
+            user = userRepository.save(user);
+
+            manager.get().addUser(user);
+            managerRepository.save(manager.get());
+            return  new UserResponse(user);
+        }else{
+            throw new Exception("User not created");
+        }
     }
 }
